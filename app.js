b5aea0fcfd11c26bc51086131de916bd218bbca0
Wolf <d.wolftripp@gmail.com>
2022-07-26 23:47:26 -0700

    fix expression / declaration

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ Exercise 2:
   - Define a function as a function expression, `maxOfThree`, that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
 */
 
-function maxOfThree(num1, num2, num3){
+var maxOfThree = function (num1, num2, num3){
   if (num1 > num2 && num1 > num3){
     return num1
   } else if (num2 > num1 && num2 > num3){
@@ -65,7 +65,7 @@ Exercise 4:
   - Define a function, as a function expression, `sumArray` that takes an array of numbers and returns the sum of those numbers. For example, `sumArray([2, 4, 5]);` would return `11`.
 */
 
-function sumArray(arr){
+var sumArray = function (arr){
   return arr.reduce((num1, num2) => num1 + num2)
 }
 
