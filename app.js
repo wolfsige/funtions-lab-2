41543419c219fad71f8ecb66434b5ec1465061fd
Wolf <d.wolftripp@gmail.com>
2022-07-30 13:15:21 -0700

    complete exercise 9

--- app.js
+++ app.js
@@ -131,7 +131,11 @@ Exercise 9:
   - Define a function, as a function declaration, `stringsLongerThan` that takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, `stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);` would return `["hello", "morning"]`.
 */
 
-
+function stringsLongerThan(arr, num){
+  return arr.filter(str => {
+    return str.length > num
+  })
+}
 
 console.log(
   "Exercise 9 Result:\n",
